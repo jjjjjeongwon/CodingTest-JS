520af3329877a68ab3d2b4368e173b6391eed6a5
jjjjjeongwon <jjjjjwon21@gmail.com>
2024-05-06 23:19:32 +0900

    complete

---
+++ 최대공약수와 최소공배수/script.js
@@ -0,0 +1,11 @@
+let getGCD = (num1, num2) => {
+  let gcd = 1;
+
+  for (let i = 2; i <= Math.min(num1, num2); i++) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      gcd = i;
+    }
+  }
+
+  return gcd;
+};
