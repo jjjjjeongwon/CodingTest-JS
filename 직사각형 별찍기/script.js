4058253262644c9ad229a6fe80b854f321e8ddec
jjjjjeongwon <jjjjjwon21@gmail.com>
2024-06-17 17:10:25 +0900

    complete

---
+++ 직사각형 별찍기/script.js
@@ -0,0 +1,8 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  const row = `${"*".repeat(a)}\n`; // *이 a번 반복
+  console.log(row.repeat(b)); // row를 b번 반복
+});
